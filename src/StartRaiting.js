7a1f41948faf4154e89bb64fd2ad5e6f06cd8b45
Sebastian Manco Gomez <sebasmg1393@gmail.com>
2023-06-25 22:54:35 -0500

    seb: hover effects in start components is ok

--- src/StartRaiting.js
+++ src/StartRaiting.js
@@ -15,6 +15,7 @@ const styleText = { lineHeight: '1', margin: '0' };
 
 function StartRaiting({ maxRating = 5 }) {
   const [rating, setReating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
 
   const handlerRating = function (rating) {
     setReating(ratingSnapshot => ratingSnapshot + rating);
@@ -25,13 +26,15 @@ function StartRaiting({ maxRating = 5 }) {
       <div style={startContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
-            full={rating >= i + 1}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
             key={i}
             onRate={() => handlerRating(i + 1)}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
           ></Star>
         ))}
       </div>
-      <p style={styleText}>{rating || ''}</p>
+      <p style={styleText}>{tempRating || rating || ''}</p>
     </div>
   );
 }
@@ -45,10 +48,16 @@ const startStyle = {
   cursor: 'pointer  ',
 };
 
-function Star({ onRate, full }) {
+function Star({ onRate, full, onHoverIn, onHoverOut }) {
   return (
-    <span style={startStyle} onClick={onRate}>
+    <span
+      style={startStyle}
+      onClick={onRate}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+    >
       {full ? (
+        // FULL STAR
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
@@ -58,6 +67,7 @@ function Star({ onRate, full }) {
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
       ) : (
+        // EMPTY STAR
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
